1f3e8fd34aebf2cd60256a1b7b49191480166c24
Åsmund Myklebust <asmundmyklebust1@gmail.com>
2023-10-08 20:04:47 +0200

    RVR-control
    Created .hpp and .cpp file for RVR-control

---
+++ src/RVR-Control/rvr_controller.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Admin on 08/10/2023.
+//
+
+#include "rvr_controller.h"
+#include "sphero_sdk.h"
+
+RVRController::RVRController() {
+    // Constructor implementation
+}
+
+RVRController::~RVRController() {
+    // Destructor implementation
+}
+
+void RVRController::initialize() {
+    // Initialization code
+}
+
+void RVRController::setSpeed(int speed) {
+    // Set speed code
+}
---
+++ src/RVR-Control/rvr_controller.h
@@ -0,0 +1,21 @@
+//
+// Created by Admin on 08/10/2023.
+//
+
+#ifndef AIS2203_PORTFOLIO_RVR_CONTROLLER_H
+#define AIS2203_PORTFOLIO_RVR_CONTROLLER_H
+
+class RVRController {
+public:
+    RVRController();
+    ~RVRController();
+
+    void initialize();
+    void setSpeed(int speed);
+    // ... other member functions ...
+
+private:
+    // ... private member variables ...
+};
+
+#endif //AIS2203_PORTFOLIO_RVR_CONTROLLER_H
