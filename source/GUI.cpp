07564433dd058f89a6b86ebfb36bd62534636303
Markus Lyngstad <markuslyngstad@gmail.com>
2023-11-20 11:51:21 +0100

    added source file and header for GUI in folders

---
+++ source/GUI.cpp
@@ -0,0 +1,12 @@
+// std libraries
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// opencv libraries
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+
+// project headers
+#include "GUI.h"
